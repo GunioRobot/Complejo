0cd19d36485feab9ba748f93ac3dfc459c3a76a2
Alfredo Villagran Olguin <ketzerato@gmail.com>
2011-08-28 09:53:54 -0500

    first commit

---
+++ Complejo/Complejo.cpp
@@ -0,0 +1,9 @@
+//
+//  Complejo.cpp
+//  Complejo
+//
+//  Created by ALFREDO VILLAGRAN OLGUIN on 8/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
---
+++ Complejo/Complejo.h
@@ -0,0 +1,14 @@
+//
+//  Complejo.h
+//  Complejo
+//
+//  Created by ALFREDO VILLAGRAN OLGUIN on 8/28/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef Complejo_Complejo_h
+#define Complejo_Complejo_h
+
+
+
+#endif
